dd173f33272c848abb14f3e3c37de159aa5ebf90
S1nOfSloth <151624696+S1nOfSloth@users.noreply.github.com>
2024-03-02 16:31:43 +1000

    Update Лаба 2.c

--- Лаба 2.c
+++ Лаба 2.c
@@ -1,38 +1,48 @@
-#include <stdio.h>
-
 int main()
 {
-  double matrix[3][3];
-  int i, j;
-  double main_diagonal = 0;
-  double side_diagonal = 0;
-  printf("Введите значения элементов матрицы 3 на 3:\n");
-  for (i = 0; i < 3; i++){
-    for (j = 0; j < 3; j++){
-      scanf("%lf", &matrix[i][j]);
-      if (i == j){
-        main_diagonal += matrix[i][j];
+    int x, y;
+    int i, j;
+    double main_diagonal = 0;
+    double side_diagonal = 0;
+    printf("Введите размерность матрицы:\n");
+    scanf("%d %d", &x, &y);
+    double matrix[x][y];
+    
+    printf("Введите значения элементов матрицы i x j:\n");
+      for (i = 0; i < x; i++){
+        for (j = 0; j < y; j++){
+          scanf("%lf", &matrix[i][j]);
+          if (i == j){
+            main_diagonal += matrix[i][j];
+          }
+          if (j == x - i - 1){
+            side_diagonal += matrix[i][j];
+          }     
+        }
       }
-      if (i + j == 2){
-        side_diagonal += matrix[i][j];
-      }     
+    printf("Матрица:\n");
+    for (i = 0; i < x; i++){
+        for (j = 0; j < y; j++){
+            printf("%.0lf ", matrix[i][j]);
+        
+        }
+        printf("\n");
     }
-  }
-  printf("Сумма элементов на главной диагонали: %.1lf\n", main_diagonal);
-  printf("Сумма элементов на побочной диагонали: %.1lf\n", side_diagonal);
-
-  int matrix_1[2][2];
-  printf("Введите значения элементов матрицы 2 на 2:\n");
-  for (i = 0; i < 2; i++){
-    for (j = 0; j < 2; j++){
-      scanf("%d", &matrix_1[i][j]);
-    }
-  }
-  printf("Квадрат матрицы:\n");
-  for (i = 0; i < 2; i++){
-    for (j = 0; j < 2; j++){
-      printf("%d ", matrix_1[i][j] * matrix_1[i][j]);
-    }
-  }
-  return 0;
+    printf("Сумма элементов на главной диагонали: %.1lf\n", main_diagonal);
+    printf("Сумма элементов на побочной диагонали: %.1lf\n", side_diagonal);
+    
+    int matrix_1[2][2];
+    printf("Введите значения элементов матрицы 2 на 2:\n");
+    for (i = 0; i < 2; i++){
+        for (j = 0; j < 2; j++){
+          scanf("%d", &matrix_1[i][j]);
+        }
+      }
+    printf("Квадрат матрицы:\n");
+    for (i = 0; i < 2; i++){
+        for (j = 0; j < 2; j++){
+          printf("%d ", matrix_1[i][j] * matrix_1[i][j]);
+        }
+      }
+    return 0;
 }
