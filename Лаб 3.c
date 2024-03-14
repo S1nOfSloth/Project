44fcd9c01ea43b76494516a6251d4ce932f7484e
S1nOfSloth <151624696+S1nOfSloth@users.noreply.github.com>
2024-03-14 18:58:28 +1000

    Update Лаб 3.c

--- Лаб 3.c
+++ Лаб 3.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
-int main(){
-  int mass[80];
-  
 
+int main(){
+    char mass[80];
+    
+    printf("Введите строку до 80 символов: ");
+    scanf("%s", mass);
+  
+    for (int i = 0; i < 80; i++){
+        if (mass[i] == 'a'){
+            mass[i] = 'A';
+        }
+        else if (mass[i] == 'b'){
+            mass[i] = 'B';
+        }
+    }
+    printf("Итоговая строка: ");
+    printf("%s", mass);
+  
   return 0;
 }
