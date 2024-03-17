94465be0f50070d10ec847d0941c70c528e8f36e
S1nOfSloth <151624696+S1nOfSloth@users.noreply.github.com>
2024-03-17 17:19:08 +1000

    Update Лаб 4.c

--- Лаб 4.c
+++ Лаб 4.c
@@ -2,13 +2,17 @@
 #include <math.h>
 int main()
 {
-  double a, b, c;
+  double a, b, c, perimeter, area, perimeter_0;
   printf("Введите стороны треугольника: ");
   scanf("%lf %lf %lf", &a, &b, &c);
   if (a < b + c && b < a + c && c < a + b)
   {
-    printf("%d", )
+    perimeter = a + b + c;
+    perimeter_0 = perimeter / 2;
+    printf("Периметр треугольника: %.2lf\n", perimeter);
+    printf("Площадь треугольника: %.2lf", sqrt(perimeter_0*(perimeter_0 - a)*(perimeter_0 - b)*(perimeter_0 - c)));
   }
-    
+  else 
+    printf("Треугольника с данными сторонами не существует.");
   return 0;
 }
