58e62385fe20eb8c83faa224925944d439a5dad0
S1nOfSloth <151624696+S1nOfSloth@users.noreply.github.com>
2024-03-17 17:04:44 +1000

    Update Лаб 4.c

--- Лаб 4.c
+++ Лаб 4.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #include <math.h>
-int main(){
-  
-  
-  
+int main()
+{
+  double a, b, c;
+  printf("Введите стороны треугольника: ");
+  scanf("%lf %lf %lf", &a, &b, &c);
+  if (a < b + c && b < a + c && c < a + b)
+  {
+    printf("%d", )
+  }
+    
   return 0;
 }
