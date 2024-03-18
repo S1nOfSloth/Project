fe9281b22284e539a8c2569079945e8e556290f4
S1nOfSloth <151624696+S1nOfSloth@users.noreply.github.com>
2024-03-19 01:47:31 +1000

    Update Лаб 4.c

--- Лаб 4.c
+++ Лаб 4.c
@@ -1,18 +1,36 @@
+//main.c
 #include <stdio.h>
-#include <math.h>
+#include "second.h"
+
 int main()
 {
-  double a, b, c, perimeter, area, perimeter_0;
-  printf("Введите стороны треугольника: ");
-  scanf("%lf %lf %lf", &a, &b, &c);
-  if (a < b + c && b < a + c && c < a + b)
-  {
-    perimeter = a + b + c;
-    perimeter_0 = perimeter / 2;
-    printf("Периметр треугольника: %.2lf\n", perimeter);
-    printf("Площадь треугольника: %.2lf", sqrt(perimeter_0*(perimeter_0 - a)*(perimeter_0 - b)*(perimeter_0 - c)));
-  }
-  else 
-    printf("Треугольника с данными сторонами не существует.");
-  return 0;
+    float a1, b1, c1;
+    printf("Введите стороны треугольника: ");
+    scanf("%f %f %f", &a1, &b1, &c1);
+    
+    if (a1 < b1 + c1 && b1 < a1 + c1 && c1 < a1 + b1)
+    {
+        float P = Perimeter(a1, b1, c1);
+        float A = Area(a1, b1, c1);
+        printf("Периметр треугольника: %.2f\n", P);
+        printf("Площадь треугольника: %.2f", A);
+    }
+    else
+    {
+        printf("Треугольника с данными сторонами не существует!");
+    }
+
+    return 0;
+}
+//second.h
+#include <math.h>
+
+float Perimeter(float a, float b, float c)
+{
+    return a+b+c;
+}
+float Area(float a, float b, float c)
+{
+    float p = (a+b+c)/2.;
+    return sqrt(p * (p - a) * (p - b) * (p - c));
 }
