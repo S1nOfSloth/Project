ab5b79a54b5ebe985fc33d3fbeafd6ce7b4c546b
S1nOfSloth <151624696+S1nOfSloth@users.noreply.github.com>
2024-04-20 12:24:51 +1000

    Create Лаб 7.c

---
+++ Лаб 7.c
@@ -0,0 +1,39 @@
+#include <stdio.h> 
+#include <string.h> 
+ 
+struct humen{ 
+  char name[30],surname[30]; 
+  int year; 
+}; 
+ 
+int main(){ 
+  int n = 4; 
+  struct humen p[n], psorted[n], t; 
+  int i,j; 
+   
+  for( i=0; i < n; i++){ 
+    printf("Enter the %d person's name, surname and date of birth: ", i+1); 
+    scanf("%s %s %d", p[i].name, p[i].surname, &p[i].year); 
+  } 
+  
+  for ( i = 0; i < n; i++){ 
+    psorted[i] = p[i]; 
+  } 
+ 
+    for ( i = 0; i < n-1; i++){ 
+        for ( j = 0; j < n-i-1; j++) { 
+            if (psorted[j].year > psorted[j+1].year ) { 
+                t = psorted[j]; 
+                psorted[j] = psorted[j+1]; 
+                psorted[j+1] = t; 
+            } 
+        } 
+    } 
+   
+  printf("\nSorted list of people: \n"); 
+  for ( i=0; i<n; i++){ 
+    printf("%s %s %d \n", psorted[i].name, psorted[i].surname, psorted[i].year); 
+  } 
+   
+  return 0;   
+}
